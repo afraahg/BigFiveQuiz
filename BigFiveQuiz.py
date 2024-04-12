f5afca3ea07f3bf6c036d236b0efa39edb7a32c2
Afraah Goshu <afraahgoshu@wireless-10-104-135-245.umd.edu>
2024-04-12 12:42:29 -0400

    declared init methods

--- BigFiveQuiz.py
+++ BigFiveQuiz.py
@@ -1,2 +1,36 @@
 ##
 
+class UserProfile:
+    
+    def __init__(self, name, age, gender, scores):
+        
+        self.name=name
+        self.age=age
+        self.gender=gender
+        #score tracker
+        self.scores=scores
+        
+        #json profile
+        self.profileJSON={self.name:self.scores}
+        
+    def DisplayGraph():
+        
+        #pass in different values to display different type of graph?
+        #to compare # of reson
+        
+        # x axis is big five, y axis is the number of responses 
+        #histogram
+        #
+        
+        
+        pass
+    
+    def CompareUser(otherUser):
+        #json dump, load or something here
+        pass
+    
+    
+
+class BigFive:
+    def __init__(self):
+        pass
